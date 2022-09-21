4a239fe06743110334a46dbe5a3449fc7ea4501a
William Benham <william.benham77@gmail.com>
2022-09-21 13:23:20 -0600

    Initial commit

--- Pet.cs
+++ Pet.cs
@@ -8,6 +8,50 @@ namespace template_csharp_virtual_pet
 {
     public class Pet
     {
+        // properties
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Hunger { get; set; }
+        public int Boredom { get; set; }
+        public int Health { get; set; } 
+
+        // Constructors
+
+        public Pet()
+        {
+            Name = "";
+            Species = "";
+            Hunger = 60;
+            Boredom = 60;
+            Health = 60;
+        }
+
+        // Methods
+
+        public void Feed()
+        {
+            Hunger -= 10;
+        }
+
+        public void SeeDoctor()
+        {
+            Health += 30;
+        }
+
+        public void Play()
+        {
+            Hunger += 10;
+            Boredom -= 20;
+            Health += 10;
+        }
+
+        public void Tick()
+        {
+            Hunger += 5;
+            Boredom += 5;
+            Health -= 5;
+        }
 
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// Your Program Code Here
-Console.WriteLine("Welcome to Virtual Pet!");
+﻿
+using System.Runtime.InteropServices;
+using template_csharp_virtual_pet;
+
+Pet thisPet;
+thisPet = new Pet();
+bool continueRunning = true;
+while (continueRunning)
+{
+    Console.Clear();
+    Console.WriteLine("Welcome to Virtual Pet!\n");
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine("1. Create your pet.");
+    Console.WriteLine("2. See your pet's status.");
+    Console.WriteLine("3. Feed your pet.");
+    Console.WriteLine("4. Play with your pet.");
+    Console.WriteLine("5. Take your pet to the doctor.");
+    Console.WriteLine("Press Q to quit");
+
+    var playerChoice = Console.ReadLine().ToLower();
+
+    switch (playerChoice)
+    {
+        case "1":
+            Console.WriteLine("What's the name of your pet?");
+            var petName = Console.ReadLine();
+            Console.WriteLine("What species is your pet?");
+            var petSpecies = Console.ReadLine();
+            thisPet.Name = petName;
+            thisPet.Species = petSpecies;
+            break;
+        case "2":
+            Console.WriteLine($"\n Name {thisPet.Name} Species {thisPet.Species} Health {thisPet.Health} Hunger {thisPet.Hunger} Boredom {thisPet.Boredom}");
+            Console.Write("\nPress enter to return to the Main Menu");
+            Console.ReadLine();
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+        case "5":
+            break;
+        case "q":
+            continueRunning = false;
+            Console.WriteLine("Thanks for playing!");
+            Console.Beep();
+            break;
+        default:
+            break;
+
+    }
+
+} 
